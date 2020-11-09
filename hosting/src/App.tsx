4b1714db0425805aed020832f465bbad9bd81225
Merseong <esc990720@korea.ac.kr>
2020-11-09 14:43:59 +0000

    Add menu bar on top

--- hosting/src/App.tsx
+++ hosting/src/App.tsx
@@ -1,23 +1,40 @@
 import React from 'react';
-import reactLogo from './React_logo.svg';
-import firebaseLogo from './Firebase_Logo_Logomark.svg'
-import './App.css';
+import 'semantic-ui-css/semantic.min.css';
+import { 
+  Button,
+  Container,
+  Header, 
+  Icon, 
+  Input, 
+  Menu,
+} from 'semantic-ui-react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          <img src={firebaseLogo} className="App-logo" alt="logo" />
-          <img src={reactLogo} className="App-logo" alt="logo" />
-        </p>
-        <p>
-          Firebase with React-Typescript!
-        </p>
-        <p>
-          by SoolJari
-        </p>
-      </header>
+    <div>
+      <Menu fixed='top' stackable>
+        <Container>
+          <Menu.Item>
+            <Header>
+              <Icon name='glass martini'/>
+              <Header.Content>술자리</Header.Content>
+            </Header>
+          </Menu.Item>
+          <Menu.Item>
+            <Input
+              transparent
+              style={{
+                minWidth: '320px',
+              }}
+            />
+            <Button basic icon='search'/>
+          </Menu.Item>
+          <Menu.Item position='right'>
+            <Icon fitted size='large' name='user' link/>{{/** 메뉴로 변경예정, 로그인시 초록색으로 되게 */}}
+          </Menu.Item>
+          <Menu.Item fitted></Menu.Item>
+        </Container>
+      </Menu>
     </div>
   );
 }
