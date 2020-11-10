b680e2f0f24e173db8eb6e3c3b269eb34959db60
Merseong <esc990720@korea.ac.kr>
2020-11-10 23:35:30 +0900

    Add google login

--- hosting/src/App.tsx
+++ hosting/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import firebase from "firebase/app";
 import 'semantic-ui-css/semantic.min.css';
 import { 
   Button,
@@ -8,8 +9,23 @@ import {
   Input, 
   Menu,
 } from 'semantic-ui-react';
+import { LoginButton } from './component/LoginButton';
 
 function App() {
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyCgOpj7xM54wHvP3F2NMsQLEj2626BnHlE",
+    authDomain: "sooljari-alcdb.firebaseapp.com",
+    databaseURL: "https://sooljari-alcdb.firebaseio.com",
+    projectId: "sooljari-alcdb",
+    storageBucket: "sooljari-alcdb.appspot.com",
+    messagingSenderId: "536442650410",
+    appId: "1:536442650410:web:52d3f028102e6e60b96853",
+    measurementId: "G-KBRDDM5RSN"
+  }
+
+  !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
+
   return (
     <div>
       <Menu fixed='top' stackable>
@@ -30,7 +46,7 @@ function App() {
             <Button basic icon='search'/>
           </Menu.Item>
           <Menu.Item position='right'>
-            <Icon fitted size='large' name='user' link/>
+            <LoginButton/>
           </Menu.Item>
           <Menu.Item fitted></Menu.Item>
         </Container>
---
+++ hosting/src/component/LoginButton.tsx
@@ -0,0 +1,98 @@
+import React from "react";
+import firebase from "firebase/app";
+import "firebase/auth";
+import {
+    Button,
+    Icon 
+} from "semantic-ui-react";
+
+export interface LoginButtonProps {
+}
+
+interface LoginButtonState {
+    isLogin: boolean,
+    isAdmin: boolean,
+}
+
+export class LoginButton extends React.Component<LoginButtonProps, LoginButtonState> {
+    constructor(props: LoginButtonProps) {
+        super(props);
+        this.state = {
+            isLogin: false,
+            isAdmin: false,
+        }
+    }
+
+    googleLogin() {
+        const provider = new firebase.auth.GoogleAuthProvider();
+
+        firebase.auth().signInWithPopup(provider)
+        .then(result => {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            //const credential = result.credential as firebase.auth.OAuthCredential;
+            //const token = credential.accessToken;
+            // The signed-in user info.
+            const user = result.user;
+            //console.log(token, user);
+            this.setState({
+                isLogin: true,
+                isAdmin: user?.email === 'esc990720@korea.ac.kr' || user?.email === 'esc990720@gmail.com',
+            })
+            console.log('Google Login');
+        })
+        .catch(error => {
+            // Handle Errors here.
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // The email of the user's account used.
+            const email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            const credential = error.credential;
+            console.error(errorCode, errorMessage, email, credential);
+        });
+    }
+
+    logout() {
+        if (this.state.isLogin) {
+            firebase.auth().signOut()
+            .then(() => {
+                this.setState({
+                    isLogin: false,
+                    isAdmin: false,
+                })
+                console.log('Logout');
+            })
+            .catch(e => {
+                console.error(e)
+            });
+        }
+    }
+
+    render() {
+        const { isLogin, isAdmin } = this.state;
+
+        return (
+            isLogin ? 
+                isAdmin ? 
+                    <Button 
+                        basic
+                        color='green'
+                        icon={<Icon fitted size='large' name='user'/>}
+                        onClick={() => this.logout()}
+                    />
+                    :
+                    <Button 
+                        basic
+                        color='red'
+                        icon={<Icon fitted size='large' name='user'/>}
+                        onClick={() => this.logout()}
+                    />
+                :
+                <Button 
+                    basic
+                    icon={<Icon fitted size='large' name='user'/>}
+                    onClick={() => this.googleLogin()}
+                />
+        )
+    }
+}
