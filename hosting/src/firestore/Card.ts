9755e9af0c47a4e069d27b64d7a381727e11d199
Merseong <esc990720@korea.ac.kr>
2020-11-26 23:30:17 +0900

    Wow reset!

--- hosting/src/firestore/Card.ts
+++
@@ -1,9 +0,0 @@
-export class Card {
-    title: string; // 카드의 제목
-    id: string; // 카드의 문서 아이디
-
-    constructor(id:string, title:string) {
-        this.id = id;
-        this.title = title;
-    }
-}
--- ssajiAutoset.bat
+++ ssajiAutoset.bat
@@ -5,5 +5,5 @@ git config user.email "esc990720@korea.ac.kr"
 
 :: npm settings
 cd ./hosting/
+npm install -g gulp firebase-tools
 npm i
-npx firebase login
